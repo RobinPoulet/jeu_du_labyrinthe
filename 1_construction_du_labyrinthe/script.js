0f9361add589ffdca63dd96953f6b1a1eab3ed7c
robin.poulet <robin.poulet@le-campus-numerique.fr>
2021-09-09 17:09:23 +0200

    tentative d'utilisation d'un algorithme de dfs  pas encore au point

--- 1_construction_du_labyrinthe/script.js
+++ 1_construction_du_labyrinthe/script.js
@@ -1,8 +1,8 @@
 // Liste de tous les labyrinthes
-console.log(jsonDatas);
+// console.log(jsonDatas);
 
 // Labyrinthe de taille 3x3 : ex-0
-console.log(jsonDatas[3]["ex-0"]);
+// console.log(jsonDatasBis[3]["ex-0"]);
 
 let canvas = document.getElementById("myCanvas");
 let ctx = canvas.getContext("2d");
@@ -10,7 +10,7 @@ let size = 0;
 
 let mazeSize = document.getElementById("mazeSize").value;
 let mazeEx = document.getElementById("mazeExemple").value;
-
+// let maze = jsonDatasBis[mazeSize][mazeEx];
 
 // événement qui génére la création du labyrinthe
 document.querySelector("#mazeGenerate").addEventListener('click', function () {
@@ -18,7 +18,8 @@ document.querySelector("#mazeGenerate").addEventListener('click', function () {
     mazeEx = document.getElementById("mazeExemple").value;
     size = configCellSize(mazeSize);
     ctx.clearRect(0, 0, canvas.width, canvas.height);
-    drawMaze(jsonDatas[mazeSize][mazeEx]);
+    drawMaze(jsonDatasBis[mazeSize][mazeEx]);
+    dfs(jsonDatasBis[mazeSize][mazeEx], 0, mazeSize * mazeSize - 1, mazeSize);
 
 })
 
@@ -70,13 +71,82 @@ function drawExit() {
 
 function drawMaze(maze) {
     maze.forEach(function (cell) {
-        drawCell(cell.posY * size, cell.posX * size, cell.walls);
+        cell.visited = false;
+        drawCell(cell.posX * size, cell.posY * size, cell.walls);
+        // console.log(cell);
     });
     drawStart();
     drawExit();
 }
 
-// drawMaze(jsonDatas[15]["ex-1"]);
+class Stack {
+    constructor() {
+        this.stack = [];
+    }
+
+    empty() {
+        return this.stack.length === 0;
+    }
+
+    push(item) {
+        this.stack.push(item);
+    }
+
+    pop() {
+        return this.stack.pop()
+    }
+}
+
+function unvisitedneighbors(maze, pos, size) {
+    console.log(maze[0].walls);
+    console.log(pos);
+    console.log(size);
+    let unvisitneighboors = [];
+    let sizeInt = parseInt(size);
+    if (!maze[pos].walls[0] && !maze[pos - sizeInt].visited) {
+        unvisitneighboors.push(maze[pos - sizeInt]);
+    }
+    if (!maze[pos].walls[1] && !maze[pos + 1].visited) {
+        unvisitneighboors.push(maze[pos + 1]);
+    }
+    if (!maze[pos].walls[2] && !maze[pos + sizeInt].visited) {
+        unvisitneighboors.push(maze[pos + sizeInt]);
+    }
+    if (!maze[pos].walls[3] && !maze[pos - 1].visited) {
+        unvisitneighboors.push(maze[pos - 1]);
+    }
+    return unvisitneighboors;
+}
+
+
+function dfs(maze, start, end, size) {
+    // je crée le stack et met le noeud de départ dedans
+    let stack = new Stack();
+   maze[start].visited = true;
+   stack.push(maze[start]);
+   // tant qu'il reste un noeud dans le stack je continue
+   while (!stack.empty()) {
+       let curCell = stack.pop();
+
+       // C'est gagné si je suis arrivé à la fin
+       if (curCell === maze[end]) {
+           console.log("Gagné bravo !!!");
+       }
+
+       // Je regarde les voision non visités dans l'ordre (Haut, Droite, Bas, Gauche),
+       // je marque le parent, comme visité et je l'ajoute au stack
+       let unvisiteNeighbors = unvisitedneighbors(maze, curCell, size);
+       for (let i = 0; i < unvisiteNeighbors.length; i++) {
+           unvisiteNeighbors[i].visited = true;
+           unvisiteNeighbors[i].parents = maze[curCell];
+           stack.push(unvisiteNeighbors[i]);
+           console.log("j'ai visité la case : " + maze[curCell]);
+       }
+   }
+
+}
+
+
 
 
 
