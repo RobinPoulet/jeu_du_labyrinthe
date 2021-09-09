90b1b28e9b180da06f98d8d4a06fb8b83a401d67
robin.poulet <robin.poulet@le-campus-numerique.fr>
2021-09-09 10:45:39 +0200

    fonction qui génére le labyrinthe avec une petite interface pour générer la création du labyrinthe en fonction de sa taille et de l'exemeple

--- 1_construction_du_labyrinthe/script.js
+++ 1_construction_du_labyrinthe/script.js
@@ -6,6 +6,19 @@ console.log(jsonDatas[3]["ex-0"]);
 
 let canvas = document.getElementById("myCanvas");
 let ctx = canvas.getContext("2d");
+let size = 30;
+
+let mazeSize = document.getElementById("mazeSize").value;
+let mazeEx = document.getElementById("mazeExemple").value;
+
+
+document.querySelector("#mazeGenerate").addEventListener('click', function () {
+    mazeSize = document.getElementById("mazeSize").value;
+    mazeEx = document.getElementById("mazeExemple").value;
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    drawMaze(jsonDatas[mazeSize][mazeEx]);
+
+})
 
 // je crée une fonction pour dessiner une ligne dans le canvas
 function drawLine(startX, startY, endX, endY) {
@@ -14,12 +27,49 @@ function drawLine(startX, startY, endX, endY) {
 }
 
 // je crée une fonction pour dessiner une case du labyrinthe avec la fonction drawLine pour les bordures
-function drawCell(x, y, side, walls) {
-    let up = walls[0];
+function drawCell(x, y, walls) {
+    let top = walls[0];
     let right = walls[1];
-    let down = walls[2];
+    let bottom = walls[2];
     let left = walls[3];
+
+    ctx.beginPath();
+    if (top) {
+        drawLine(x, y, x + size, y);
+    }
+    if (right) {
+        drawLine(x + size, y, x + size, y + size);
+    }
+    if (bottom) {
+        drawLine(x + size, y + size, x, y + size);
+    }
+    if (left) {
+        drawLine(x, y + size, x, y);
+    }
+    ctx.stroke();
 }
+
+function drawStart() {
+    ctx.beginPath();
+    ctx.fillStyle = '#FFA500';
+    ctx.fillRect(0, 0, size / 2, size / 2);
+}
+
+function drawExit() {
+    ctx.beginPath();
+    ctx.fillStyle = '#008000';
+    ctx.fillRect((mazeSize * size) - size/2, (mazeSize * size) - size/2, size/2, size/2);
+}
+
+function drawMaze(maze) {
+    maze.forEach(function (cell) {
+        drawCell(cell.posY * size, cell.posX * size, cell.walls);
+    });
+    drawStart();
+    drawExit();
+}
+
+// drawMaze(jsonDatas[15]["ex-1"]);
 
 
 
